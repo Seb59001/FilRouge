31595a7d706a8fa9d9bf048d1240dca4c85e8895
Sebastien <leg.sebastien@gmail.com>
2023-03-28 13:28:35 +0200

    Création entité Eleve et Creneau

---
+++ src/Entity/Creneau.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CreneauRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CreneauRepository::class)]
+class Creneau
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $libelle_jour = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heure_debut = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $heure_fin = null;
+
+    #[ORM\Column]
+    private ?int $numero_semaine = null;
+
+    #[ORM\Column]
+    private ?\DateInterval $annee_scolaire = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelleJour(): ?string
+    {
+        return $this->libelle_jour;
+    }
+
+    public function setLibelleJour(string $libelle_jour): self
+    {
+        $this->libelle_jour = $libelle_jour;
+
+        return $this;
+    }
+
+    public function getHeureDebut(): ?\DateTimeInterface
+    {
+        return $this->heure_debut;
+    }
+
+    public function setHeureDebut(\DateTimeInterface $heure_debut): self
+    {
+        $this->heure_debut = $heure_debut;
+
+        return $this;
+    }
+
+    public function getHeureFin(): ?\DateTimeInterface
+    {
+        return $this->heure_fin;
+    }
+
+    public function setHeureFin(\DateTimeInterface $heure_fin): self
+    {
+        $this->heure_fin = $heure_fin;
+
+        return $this;
+    }
+
+    public function getNumeroSemaine(): ?int
+    {
+        return $this->numero_semaine;
+    }
+
+    public function setNumeroSemaine(int $numero_semaine): self
+    {
+        $this->numero_semaine = $numero_semaine;
+
+        return $this;
+    }
+
+    public function getAnneeScolaire(): ?\DateInterval
+    {
+        return $this->annee_scolaire;
+    }
+
+    public function setAnneeScolaire(\DateInterval $annee_scolaire): self
+    {
+        $this->annee_scolaire = $annee_scolaire;
+
+        return $this;
+    }
+}
---
+++ src/Entity/Eleve.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\EleveRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: EleveRepository::class)]
+class Eleve
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $telephone = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $sexe = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $niveau_etude = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $email = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): self
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(string $prenom): self
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getTelephone(): ?string
+    {
+        return $this->telephone;
+    }
+
+    public function setTelephone(string $telephone): self
+    {
+        $this->telephone = $telephone;
+
+        return $this;
+    }
+
+    public function getSexe(): ?string
+    {
+        return $this->sexe;
+    }
+
+    public function setSexe(string $sexe): self
+    {
+        $this->sexe = $sexe;
+
+        return $this;
+    }
+
+    public function getNiveauEtude(): ?string
+    {
+        return $this->niveau_etude;
+    }
+
+    public function setNiveauEtude(string $niveau_etude): self
+    {
+        $this->niveau_etude = $niveau_etude;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+}
