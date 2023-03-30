f70e4277afcbbb995a16299e04525ecb82cf11da
Sebastien <leg.sebastien@gmail.com>
2023-03-30 10:56:12 +0200

    Intégration de la navbar dans les partials

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,6 +1,6 @@
 (function () {
-   ("#msbo").on("click", function () {
-      ("body").toggleClass("msb-x");
+   $("#msbo").on("click", function () {
+      $("body").toggleClass("msb-x");
     });
   })();
   
