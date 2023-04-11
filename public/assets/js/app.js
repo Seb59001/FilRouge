8677b0f366e2d939155c8dc7fe4ebfdadb647651
Sebastien <leg.sebastien@gmail.com>
2023-04-11 08:12:37 +0200

    Call Ajax sur la suppression des éléments

---
+++ public/assets/js/app.js
@@ -0,0 +1,21 @@
+(function($){
+
+    $('.btn-warning').on('click', function(e) {
+        e.preventDefault();
+        var $a = $(this);
+        var url = $a.attr('href');
+        // $a.text('Chargement');
+        $.ajax(url, {
+            success: function() {
+                var res = confirm("Êtes-vous sûr de vouloir supprimer?");
+                if(res){
+                    $a.parents('tr').fadeOut();
+                }
+            },
+            error: function(jqxhr){
+                $a.text('supprimer')
+                alert(jqxhr.responseText);
+            }
+        });
+    });
+})(jQuery);
