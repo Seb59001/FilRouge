5718990fb63f48e1f21e50de5a54e18c376c12a7
Sebastien <leg.sebastien@gmail.com>
2023-04-13 10:36:58 +0200

    Liaison Calendar BDD

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -23,94 +23,59 @@
 
 
 
-document.addEventListener('DOMContentLoaded', function() {
-    var initialLocaleCode = 'fr';
-    var localeSelectorEl = document.getElementById('locale-selector');
-    var calendarEl = document.getElementById('calendar');
+// document.addEventListener('DOMContentLoaded', function() {
+//     var initialLocaleCode = 'fr';
+//     var localeSelectorEl = document.getElementById('locale-selector');
+//     var calendarEl = document.getElementById('calendar');
 
-    var calendar = new FullCalendar.Calendar(calendarEl, {
-        buttonText: {
-            prev: 'Précédent',
-            next: 'Suivant',
-            today: 'Aujourd\'hui',
-            year: 'Année',
-            month: 'Mois',
-            week: 'Semaine',
-            day: 'Jour',
-            list: 'Planning',
-        },
-        headerToolbar: {
-            left: 'prev,next today',
-            center: 'title,addEventButton',
-            right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
-        },
-        customButtons: {
-            addEventButton: {
-                text: 'Ajouter un événement',
-                click: function() {
-                    var dateStr = prompt('Entrez une date au format YYYY-MM-DD.');
-                    var time = prompt("Saisissez l'heure de l'événement")
-                    var date = new Date(dateStr + 'T' + time); // will be in local time
+//     var calendar = new FullCalendar.Calendar(calendarEl, {
+//         buttonText: {
+//             prev: 'Précédent',
+//             next: 'Suivant',
+//             today: 'Aujourd\'hui',
+//             year: 'Année',
+//             month: 'Mois',
+//             week: 'Semaine',
+//             day: 'Jour',
+//             list: 'Planning',
+//         },
+//         headerToolbar: {
+//             left: 'prev,next today',
+//             center: 'title,addEventButton',
+//             right: 'dayGridMonth,timeGridWeek,timeGridDay,listMonth'
+//         },
 
-                    if (!isNaN(date.valueOf())) { // valid?
-                        calendar.addEvent({
-                            title: prompt("Saisissez le nom de l'événement"),
-                            start: date,
-                            allDay: false
-                        });
-                        alert('Super, maintenant mettez à jour votre BDD');
-                    } else {
-                        alert('Date invalide');
-                    }
-                }
-            }
-        },
-        eventSources: [
+//         weekText: 'Sem.',
+//         weekTextLong: 'Semaine',
+//         allDayText: 'Toute la journée',
+//         moreLinkText: 'en plus',
+//         noEventsText: 'Aucun évènement à afficher',
+//         locale: 'fr',
+//         local: 'fr',
+//         buttonIcons: false, // show the prev/next text
+//         weekNumbers: true,
+//         navLinks: true, // can click day/week names to navigate views
+//         editable: true,
+//         dayMaxEvents: true, // allow "more" link when too many events
+//         events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day'
+//     });
 
-            // your event source
-            {
-                url: '/myfeed.php', // use the `url` property
-                color: 'yellow',    // an option!
-                textColor: 'black'  // an option!
-            }
+//     calendar.render();
 
-            // any other sources...
+//     // build the locale selector's options
+//     calendar.getAvailableLocaleCodes().forEach(function(localeCode) {
+//         var optionEl = document.createElement('option');
+//         optionEl.value = localeCode;
+//         optionEl.selected = localeCode === initialLocaleCode;
+//         optionEl.innerText = localeCode;
+//         localeSelectorEl.appendChild(optionEl);
+//     });
 
-        ],
+//     // when the selected option changes, dynamically change the calendar option
+//     localeSelectorEl.addEventListener('change', function() {
+//         if (this.value) {
+//             calendar.setOption('local', 'fr');
+//         }
+//     });
 
-
-
-        weekText: 'Sem.',
-        weekTextLong: 'Semaine',
-        allDayText: 'Toute la journée',
-        moreLinkText: 'en plus',
-        noEventsText: 'Aucun évènement à afficher',
-        locale: 'fr',
-        local: 'fr',
-        buttonIcons: false, // show the prev/next text
-        weekNumbers: true,
-        navLinks: true, // can click day/week names to navigate views
-        editable: true,
-        dayMaxEvents: true, // allow "more" link when too many events
-        events: 'https://fullcalendar.io/api/demo-feeds/events.json?overload-day'
-    });
-
-    calendar.render();
-
-    // build the locale selector's options
-    calendar.getAvailableLocaleCodes().forEach(function(localeCode) {
-        var optionEl = document.createElement('option');
-        optionEl.value = localeCode;
-        optionEl.selected = localeCode === initialLocaleCode;
-        optionEl.innerText = localeCode;
-        localeSelectorEl.appendChild(optionEl);
-    });
-
-    // when the selected option changes, dynamically change the calendar option
-    localeSelectorEl.addEventListener('change', function() {
-        if (this.value) {
-            calendar.setOption('local', 'fr');
-        }
-    });
-
-});
+// });
